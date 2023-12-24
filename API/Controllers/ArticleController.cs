005faf8f3fe7170c036251270d801289581a5f7b
Yemeth <megabyte135@gmail.com>
2023-12-25 02:37:20 +0600

    Scaffolding repositories

---
+++ API/Controllers/ArticleController.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ArticleController : ControllerBase
+    {
+        private readonly NewsDbContext _dbContext;
+        public ArticleController(NewsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ActionResult<IEnumerable<Article>> GetAll()
+        {
+            return new List<Article>();
+        }
+
+        public ActionResult<Article> Get(int id)
+        {
+            return new Article();
+        }
+
+        public ActionResult Post()
+        {
+            
+        }
+
+        public ActionResult Delete()
+        {
+
+        }
+    }
+}
